89831d2251d8254bfb604198ef0625e8aa280cdf
jabberwockie <camila.vigneaux@gmail.com>
2018-06-26 18:12:45 -0400

    fixing nav and navbar color (ok not)

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  src: string = '../assets/Logo_transp.png'
   constructor() { }
 
   ngOnInit() {
