d836052c958c95817fffd2757c1f3ee9dbf6c379
jabberwockie <camila.vigneaux@gmail.com>
2018-06-28 13:01:04 -0400

    footer grph done, new scss variable

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  titles: Array<string> = ['Links', 'Redes Sociales', '¡Contáctanos!'];
+  links: Array<string> = ['Link Uno', 'Link Dos', 'Link 3'];
+  contacto: Array<string> = ['+56 9 1234 6548', 'fundacion@fundacion.cl', 'Una Dirección #1234'];
+  facebook = 'https://facebook.com';
+  twitter = 'https://www.twitter.com';
   src = './assets/Logo_sinletras.png';
+
+
   constructor() { }
 
   ngOnInit() {
