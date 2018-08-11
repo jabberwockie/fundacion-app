e8519b7adb866acb8ba5e23b604d475224f90f47
jabberwockie <camila.vigneaux@gmail.com>
2018-08-11 14:29:42 -0600

    contacto in progress

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -9,6 +9,11 @@ export class ContactoComponent implements OnInit {
   mail = 'fundacioninclusiontea@gmail.com';
   fono = '+56 9 6540 2598';
   direccion = 'Una Dirección #123';
+  datos_bancarios = 'Fundación Inclusión TEA\t' +
+    'Banco Estado\n' +
+    'Chequera Electrónica\n' +
+    '26-9701-65541';
+  intro = 'Queremos que seas parte de nuestro proyecto. ¡Ayúdanos a seguir adelante!';
 
   constructor() { }
 
