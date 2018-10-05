de3904f148d2536cb4d95541091f03619a2294dd
jabberwockie <camila.vigneaux@gmail.com>
2018-10-05 13:40:43 -0500

    quienes somos done

--- src/app/quienes-somos/sobre-nosotros/personal.service.ts
+++ src/app/quienes-somos/sobre-nosotros/personal.service.ts
@@ -7,15 +7,15 @@ export class PersonalService {
     new Personal('Dominique Goretti', 'Muñoz Santelices', 'Psicopedagoga', 'Universidad Santo Tomás',
       'Directora', 'Co fundadora de Fundación Inclusión TEA. Lorem ipsum dolor sit amet, consecteur sdipiscing elit. ' +
       'Pellentesque eu dapibus nunc. Vestibulum venenatis sollicitudin purus, ac malesuada leo congue vel. ' +
-      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/400/250?image=39'),
+      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/300/300?image=39'),
     new Personal('Dominique Goretti', 'Muñoz Santelices', 'Psicopedagoga', 'Universidad Santo Tomás',
       'Directora', 'Co fundadora de Fundación Inclusión TEA. Lorem ipsum dolor sit amet, consecteur sdipiscing elit. ' +
       'Pellentesque eu dapibus nunc. Vestibulum venenatis sollicitudin purus, ac malesuada leo congue vel. ' +
-      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/400/250?image=39'),
+      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/300/300?image=49'),
     new Personal('Dominique Goretti', 'Muñoz Santelices', 'Psicopedagoga', 'Universidad Santo Tomás',
       'Directora', 'Co fundadora de Fundación Inclusión TEA. Lorem ipsum dolor sit amet, consecteur sdipiscing elit. ' +
       'Pellentesque eu dapibus nunc. Vestibulum venenatis sollicitudin purus, ac malesuada leo congue vel. ' +
-      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/400/250?image=39'),
+      'Praesent congue a ligula quis quismod.', 'https://picsum.photos/300/300?image=59'),
   ];
 
 
